e4ecdf4e1c0774432ba292bee5d0ac7163a02b90
iarivera <iariveradev@outlook.com>
2025-10-01 00:19:18 -0400

    created new branch for implementation of vendingmachine.lua

---
+++ vendingmachine.lua
@@ -0,0 +1,11 @@
+SMODS.Joker {
+    key = "vendingmachine",
+    atlas = 'Rivi',
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    rarity = 3,
+    cost = 7
+}
